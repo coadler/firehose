ee0cbd22298e539eb28ba40bcbc63fdb5e6ec567
Colin <colin.adler@yahoo.com>
2018-08-06 01:19:27 -0500

    lmao nice token

--- config/dev.exs
+++
@@ -1,8 +0,0 @@
-use Mix.Config
-
-# Log debug messages
-config :logger, level: :debug
-
-# Discord token
-config :firehose,
-  discord_token: "Bot MzE5NTcxNDk1NjY2MDU3MjI3.Dkl4tA.j-huf34BmP2oBKVVsD5NMirwrB4"
